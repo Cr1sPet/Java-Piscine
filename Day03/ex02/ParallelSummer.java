1741fa1882f81ec98ac0278c56475973995173d6
Josue Chopped <jchopped@va-c3.kzn.21-school.ru>
2022-07-22 18:19:34 +0300

    ex02 ok

---
+++ Day03/ex02/ParallelSummer.java
@@ -0,0 +1,78 @@
+import javax.xml.ws.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ParallelSummer {
+
+    private int threadsCount;
+    private int arrayLength;
+    private int step;
+    private int []arr;
+    Object mutex = new Object();
+
+    private static int returnSum = 0;
+    public ParallelSummer(int array[], int threadsCount) {
+
+        this.arr = array;
+        this.threadsCount = threadsCount;
+        arrayLength = array.length;
+
+        step = computeStep() - 1;
+
+    }
+
+    public int computeStep() {
+
+        int temp = arrayLength;
+        for (int i = 0; (++temp) % threadsCount != 0; i++) {
+            i++;
+        }
+        return  temp / threadsCount;
+    }
+
+
+    public int findArraySizeInInterval(int array[], int from, int to) {
+        int sum = 0;
+        for (int i = from; i <= to; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public int getSum() throws ExecutionException, InterruptedException {
+
+        List<Future> futureList = new ArrayList<>();
+
+        ExecutorService service = Executors.newFixedThreadPool(threadsCount);
+
+        for (int i = 0; i < arrayLength ; ) {
+            int to = i + step;
+            if (to >= arrayLength) {
+                to = arrayLength - 1;
+            }
+
+            int finalTo = to;
+            int finalI = i;
+
+            Future future = service.submit(() -> {
+                int sum = findArraySizeInInterval(arr, finalI, finalTo);
+                synchronized (mutex) {
+                    returnSum += sum;
+                }
+                System.out.printf("Thread-%s from %d to %d sum is %d\n",
+                        Thread.currentThread().getName().split("-")[3], finalI, finalTo, sum);
+            });
+            futureList.add(future);
+            i = ++i + step;
+        }
+
+        for (Future future : futureList) {
+            future.get();
+        }
+
+        service.shutdown();
+
+        return returnSum;
+    }
+}
--- Day03/ex02/Program.java
+++ Day03/ex02/Program.java
@@ -1,2 +1,82 @@
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
 public class Program {
+
+    public static Integer arraySize;
+    public static Integer threadsCount;
+
+    public static Integer MAX_VALUE = 1000;
+    public static Integer MIN_VALUE = -1000;
+
+
+    public static final String PARAMETER_1 = "--arraySize";
+    public static final String PARAMETER_2 = "--threadsCount";
+
+    public static int checkParameters(String parameter, String input) {
+
+        String[] inputArray = input.split("=");
+        if (inputArray.length != 2 || !inputArray[0].equals(parameter)) {
+            System.err.println("Error : bad arguments");
+            System.exit(-1);
+        }
+        try {
+            return Integer.parseInt(inputArray[1]);
+        } catch (NumberFormatException e) {
+            System.err.println("Error : bad arguments");
+            System.exit(-1);
+        }
+        return -1;
+    }
+
+    public static void parseArgs(String[] args) {
+
+        if (args.length != 2) {
+            System.err.println("Error : invalid arguments length");
+            System.exit(-1);
+        }
+        arraySize = checkParameters(PARAMETER_1, args[0]);
+        threadsCount = checkParameters(PARAMETER_2, args[1]);
+
+    }
+
+
+    public static int[] generateArray() {
+        int [] array = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = new Random().ints(MIN_VALUE, MAX_VALUE)
+                    .findFirst()
+                    .getAsInt();
+        }
+        return array;
+    }
+
+    public static int simpleSum(int []array) {
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        parseArgs(args);
+
+        int [] array = generateArray();
+
+        int sum = simpleSum(array);
+
+        System.out.printf("Sum: %d\n", sum);
+
+        ParallelSummer ps = new ParallelSummer(array, threadsCount);
+
+        sum = ps.getSum();
+
+        System.out.println("Sum by threads: " + sum);
+
+    }
+
 }
