b638887e51b9c5ad5aabfea3ca7ea7dfafc68d6a
CrsPet <serezha.petrov.02@gmail.com>
2022-07-27 12:46:52 +0300

    D05 ex01 OK

---
+++ Day06/ex01/Tests/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (name, price) VALUES ('ball', 150);
+INSERT INTO product (name, price) VALUES ('sneakers', 500);
+INSERT INTO product (name, price) VALUES ('shorts', 300);
+INSERT INTO product (name, price) VALUES ('T-shirt', 450);
+INSERT INTO product (name, price) VALUES ('backpack', 300);
