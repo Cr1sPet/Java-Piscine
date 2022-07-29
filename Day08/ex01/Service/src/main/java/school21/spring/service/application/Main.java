19f247ead3fed77e602ce5d56d211766382afa8d
Josue Chopped <jchopped@va-c3.kzn.21-school.ru>
2022-07-29 19:52:05 +0300

    ex01 OK

--- Day08/ex01/Service/src/main/java/school21/spring/service/application/Main.java
+++ Day08/ex01/Service/src/main/java/school21/spring/service/application/Main.java
@@ -1,32 +1,20 @@
 package school21.spring.service.application;
 
-import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import school21.spring.service.models.User;
-import school21.spring.service.repositories.CrudRepository;
 import school21.spring.service.repositories.UsersRepository;
-import school21.spring.service.repositories.UsersRepositoryJdbcImpl;
 
-import javax.sql.DataSource;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main
 {
-
-    private static String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static String USER = "postgres";
-    private static String PASS = "";
-
-
-
     public static void main( String[] args )
     {
-        HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl(DB_URL);
-        ds.setUsername(USER);
-        ds.setPassword(PASS);
 
-        UsersRepository repository = new UsersRepositoryJdbcImpl(ds);
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+
+        UsersRepository repository = context.getBean("usersRepositoryJdbcTemplate", UsersRepository.class);
 
         for(Long i = 0L; i < 10; i++) {
             User user = new User((i + 1), "email" + i);
